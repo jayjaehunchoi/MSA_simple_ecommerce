7d93844d7e50d5e4761e297d1ddfb0b600bfde2b
jayjaehunchoigit config --global user.name jayjaehunchoigit config --global user.name jayjaehunchoi <wogns0108@naver.com>
2021-12-17 14:48:23 +0900

    api gateway jwt 검증 필터 추가

--- api-gateway/src/main/java/com/example/cloud/config/FilterConfig.java
+++ api-gateway/src/main/java/com/example/cloud/config/FilterConfig.java
@@ -1,4 +1,4 @@
-package com.example.cloud;
+package com.example.cloud.config;
 
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
--- api-gateway/src/main/java/com/example/cloud/filter/CustomFilter.java
+++ api-gateway/src/main/java/com/example/cloud/filter/CustomFilter.java
@@ -1,4 +1,4 @@
-package com.example.cloud;
+package com.example.cloud.filter;
 
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
